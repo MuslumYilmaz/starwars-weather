fc6705ecc031301173f45c4190cfa7bfb4b421aa
MuslumYilmaz <mslmyilmaz34@gmail.com>
2021-10-21 11:12:05 +0300

    finishing touches 2

--- starwars-weather-app/src/WeatherData.js
+++ starwars-weather-app/src/WeatherData.js
@@ -1,18 +1,29 @@
 import { Component } from 'react';
 import './WeatherData.css';
-import coruscant from './images/coruscant.jpg';
+import Coruscant from './images/coruscant.jpg';
+import Hoth from './images/hoth.jpg';
+import Mustafar from './images/mustafar.webp';
+import Naboo from './images/naboo.jpg';
+import Endor from './images/endor.jpg';
+import Tatooine from './images/tattoine.jpg';
+
 
 class WeatherData extends Component {
     constructor(props) {
         super(props);
         this.state = {
             value: '',
-            setTitle: ''
+            setTitle: '',
+            display: false,
+            image: '',
+            galaxy : '',
+            response : '',
+            description : ''
         }
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);  
-    
+ 
     }
 
     handleChange(event) {
@@ -30,35 +41,59 @@ class WeatherData extends Component {
        .then(res => res.json())
        .then(
          (result) => {
-             console.log(result);
+             console.log(result.weather[0]);
              let weather = Math.floor(result.main.feels_like);
              let city = result.name;
+             let description = result.weather[0].description;
 
-             this.galaxies(weather, city);
+             this.galaxies(weather, city, description);
          })
     }
 
-    galaxies = (weather, city) => {
-        console.log(weather, city);
+    galaxies = (weather, city, description) => {
+        console.log(weather, city, description);
 
         switch (true) {
             case weather <= 0:
-                console.log(`hmm ${weather}° huh? ${city} feels like Hoth`);
+                this.setState({image: Hoth});
+                this.setState({galaxy: 'Hoth'});
+                this.setState({response: `Oh my ${weather}°? it feels like ${this.state.galaxy} out there`});
+                this.setState({description: description});
+                this.setState({display: true});
                 break;
             case weather > 0 && weather <= 15:
-                console.log(`hmm ${weather}° huh? ${city} feels like Coruscant`);
+                this.setState({image: Coruscant});
+                this.setState({galaxy: 'Coruscant'});
+                this.setState({response: `Yikes ${weather}°? it's like ${this.state.galaxy} out there`});
+                this.setState({description: description});
+                this.setState({display: true});
                 break;  
             case weather > 15 && weather <= 25:
-                console.log(`hmm ${weather}° huh? ${city} feels like Naboo`);     
+                this.setState({image: Naboo});
+                this.setState({galaxy: 'Naboo'});
+                this.setState({response: `hmm ${weather}° huh? ${city} feels like ${this.state.galaxy}`});
+                this.setState({description: description});
+                this.setState({display: true});
                 break;
             case weather > 25 && weather <= 30:
-                console.log(`hmm ${weather}° huh? ${city} feels like Endor`);     
+                this.setState({image: Endor});
+                this.setState({galaxy: 'Endor'});
+                this.setState({response: `hmm ${weather}° huh? ${city} feels like ${this.state.galaxy}`});
+                this.setState({display: true});
                 break;    
             case weather > 30 && weather <= 35:
-                console.log(`hmm ${weather}° huh? ${city} feels like Tatooine`);     
+                this.setState({image: Tatooine})
+                this.setState({galaxy: 'Tatooine'});
+                this.setState({response: `hmm ${weather}° huh? ${city} feels like ${this.state.galaxy}`});
+                this.setState({description: description});
+                this.setState({display: true});
                 break;  
             case weather >= 35:
-                console.log(`hmm ${weather}° huh? ${city} feels like Mustafar`);     
+                this.setState({image: Mustafar})
+                this.setState({galaxy: 'Mustafar'})
+                this.setState({response: `hmm ${weather}° huh? ${city} feels like ${this.state.galaxy}`});
+                this.setState({description: description});
+                this.setState({display: true});
                 break;
             default:
                 console.log(weather)
@@ -78,31 +113,34 @@ class WeatherData extends Component {
                                 <input id="icon_prefix" type="text" className="validate" value={this.state.value} onChange={this.handleChange} />
                             </div>
                             <div className="col s6 ">
-                            <a className="btn-floating pulse valign" href="/#"><i className="material-icons">cloud_queue</i></a>
+                            <button className="btn-floating pulse valign btn-large" href="/#"><i className="material-icons">cloud_queue</i></button>
+                            
                             </div>
                         </div>
                     </form>
                 </div>
 
                 <div className="row">
+                {this.state.display ? 
                     <div className="col s2 m4">
                         <div className="card  z-depth-4">
                             <div className="card-image">
-                                <img src={coruscant} alt="img"/>
-                                <span className="card-title">Card Title</span>
+                                <img src={this.state.image} alt="img"/>
+                                <span className="card-title">{this.state.galaxy}</span>
                             </div>
                             <div className="card-content">
-                                <p>I am a very simple card. I am good at containing small bits of information.
-                                I am convenient because I require little markup to use effectively.</p>
+                                <p>{this.state.response}</p>
                             </div>
                             <div className="card-action">
-                                <p>This is a link</p>
+                                <p>{this.state.description}</p>
                             </div>
                         </div>
                     </div>
+                 : null}
                 </div>
             </div>
         </div>    
+
             
         )
     }
